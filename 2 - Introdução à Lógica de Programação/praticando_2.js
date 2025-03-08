07b691848e3c7790390f7267d2578e0192b5be57
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-03-07 23:03:31 -0300

    adicionando novos arquivos e segundo exercicio de pratica

---
+++ 2 - Introdução à Lógica de Programação/praticando_2.js
@@ -0,0 +1,11 @@
+// nome, idade , numero de telefone, cpf, endereço
+let nomePessoa = "Josefa"
+let idade = 85
+let contato = "(11)12345-1234"
+let cpf = "123.123.123-12"
+let endereco = "Av São joão, 68, Bairro República"
+let recebePensao = true
+
+
+console.log("O nome da minha vó é: " + nomePessoa + " ela tem " + idade + " anos e o numero de telefone é: " + contato + 
+    " e mora no endereço " + endereco + " ela recebe pensão? " + recebePensao)
