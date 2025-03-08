c37e9ffec7b215c2cda1e490923ea2a08e82746b
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-03-07 22:37:04 -0300

    Praticando let e const

---
+++ 2 - Introdução à Lógica de Programação/praticando.js
@@ -0,0 +1,11 @@
+let poteAcucar =  "Açucar Cristal"
+let poteCafe =  "Café Pilão"
+let poteBiscoito =  "Biscoito de Maizena"
+const menssagemDavovo = "Na cozinha da vovó hoje tem: "
+
+console.log(menssagemDavovo + poteCafe + ", " + poteAcucar + ", e " + poteBiscoito)
+
+poteCafe = "Café Brasileiro"
+
+console.log(menssagemDavovo + poteCafe + ", " + poteAcucar + ", e " + poteBiscoito)
+
