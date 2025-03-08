9e1285e8011ea639f40f3c5128673164cfda8e38
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-03-07 21:38:55 -0300

    variável

---
+++ 2 - Introdução à Lógica de Programação/variavel.js
@@ -0,0 +1,9 @@
+//Saída
+console.log("Digite o nome do seu jogador: ")
+
+// Declarando variável =  let
+let nickname = "Iran"
+
+//concatenando uma mensagem fixa + uma variável
+console.log("Bem vindo, "  + nickname)
+console.log(nickname + ", Entrou no servidor!")
