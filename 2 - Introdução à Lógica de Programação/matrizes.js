ddfe186d7a099f62f465e5887ea85f22237d5c73
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-03-10 15:51:30 -0300

    Matrizes

---
+++ 2 - Introdução à Lógica de Programação/matrizes.js
@@ -0,0 +1,9 @@
+let nomesPokemon = ["Pikachu", "Charmander", "Bulbasaur"]
+
+let timePokemon = [
+    ["Pikachu", "M", "Level 1"],
+    ["Charmander", " F ", " Level 4 "]
+]
+
+console.log("O Pokemon " + timePokemon[1][0] + "É do sexo " + timePokemon[1][1] + " está no " + timePokemon[1][2])
+console.log(nomesPokemon.length)
