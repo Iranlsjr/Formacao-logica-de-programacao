a8a8caec2f07fa6cc48efb1e7cbb08a98df23c20
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-03-11 10:27:43 -0300

    Sinais  de igual e diferente

---
+++ 3 - Explorando Operadores/igual_diferente.js
@@ -0,0 +1,20 @@
+// = é Atribuição
+// == é para comparar o valor
+// === é para comparar o valor E FORMATO
+// !== é diferente
+
+//Igual
+let numero = "1"
+let resultado = numero === 1
+console.log(resultado)
+
+// Diferente 
+let marca = "Apple"
+resultado =  marca !== 1
+console.log(resultado)
+
+//Exemplo
+let cpfBloqueado = "123.456.789-10"
+let cpfUsuario = "789.454.123-10"
+let ehCPFBloquado = cpfBloqueado === cpfUsuario
+console.log(ehCPFBloquado)
