3a9f9d2e5373242e9dd4ce2aea5385f295809c75
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-03-11 16:08:15 -0300

    Repetições FOR, WHILE, DO WHILE

---
+++ 4 - Dominando Estruturas de Controle/repeticao.js
@@ -0,0 +1,30 @@
+// FOR
+for (let contator = 0; contator < 4; contator++){
+    console.log(contator)
+}
+console.log("---------")
+//Pode ser utilizado com  a letra ( I )
+for (let i = 0; i < 4; i++){
+    console.log(i)
+}
+//Exemplo 1  -Personagem com pontos de vida
+let pontosDeVida = 0
+for (let  i = 1; i  < 11; i++){
+    pontosDeVida += 1
+    console.log("Tomou poção mágica " + i)
+}
+console.log(pontosDeVida + " Totais")
+
+//WHILE
+let contator_1 = 0
+while(contator_1 < 3){
+    console.log("Ola")
+    contator_1++
+}
+
+//DO WHILE
+let contator_2 = 0
+ do {
+    console.log("ola denovo")
+    contator_2++
+ } while(contator_2 < 3)
