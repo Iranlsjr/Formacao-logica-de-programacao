7ea0edacf83f0528677b2767ac9469ffdb2a23fa
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-03-11 19:31:36 -0300

    Adicionando Funções

---
+++ 5 - Trabalhando com Funções/funcao.js
@@ -0,0 +1,18 @@
+main()
+
+function main(){
+    torrar()
+    injetarPao()
+    sendData()
+}
+function torrar(){
+    console.log("Torrando pão.")
+}
+function injetarPao(){
+    console.log("Preparando para injetar o pão.")
+    console.log("Finalizando.")
+}
+function sendData(){
+    let nomeDoBanco = "banco-de-dados"
+    console.log("Salvando em: " + nomeDoBanco)
+}
