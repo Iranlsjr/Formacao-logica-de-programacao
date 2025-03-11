f580929020b34e3d169213bf0cfb30989bec33a0
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-03-11 11:25:50 -0300

    Operadores de Maior e Menor

---
+++ 3 - Explorando Operadores/operadores_maior_e_menor.js
@@ -0,0 +1,16 @@
+// Exemplo Maior ou Igual
+let idadeMinima = 18
+let idadeUsuario = 18
+let idadePermitidaValida = idadeUsuario  >= idadeMinima
+console.log(idadePermitidaValida)
+
+//Exemplo Menor 
+let idadeCorte = 50 
+idadeUsuario = 45
+let resultadoEhTerceiraIdade = idadeCorte < idadeUsuario
+console.log(resultadoEhTerceiraIdade)
+
+//  Exemplo Texto - Case Sensitive
+let texto = "Iran"
+console.log( texto === "iran")
+
