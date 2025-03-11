b4b42143b72d7a2d631d0c3c1bd7f6ec64294fd8
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-03-11 14:19:35 -0300

    Condicionais IF, ELSE, ELSE IF

---
+++ 4 - Dominando Estruturas de Controle/condicionais.js
@@ -0,0 +1,24 @@
+let possuiOvos = false
+let itensComprados = ""
+// IF
+if(possuiOvos){
+    itensComprados = "Leite"
+}
+// ELSE 
+else{
+    console.log("passou na sessão de congelados")
+    itensComprados = "Lasanha congelada."
+}
+console.log("Item comprado: "  + itensComprados)
+// ELSE IF
+//Exemplo 
+
+let nivelDeFome = 2  
+if(nivelDeFome === 1){
+    console.log("Pouca fome")
+} else if(nivelDeFome === 2){
+    console.log("Fome média.")
+}else{
+    console.log("Muita fome.")
+}
+
