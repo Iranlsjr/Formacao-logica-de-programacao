e21829030306117cfaed0e00312caa2b89afa806
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-03-11 19:56:42 -0300

    Funcão com Parametros

---
+++ 5 - Trabalhando com Funções/funcao_com_parametro.js
@@ -0,0 +1,9 @@
+torrar("pão de forma.", "Iran", 5.90)
+torrar("pão integral.")
+
+function torrar(pao, nome = "Cliente", valor = "Consultar"){
+    console.log("Torrada feita com " + pao)
+    console.log("ela é um pedido de " + nome)
+    console.log("O valor total é de R$ " + valor)
+}
+
