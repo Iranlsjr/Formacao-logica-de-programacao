29ac4f57e2db2bff8eb0e38291d189f765024e7e
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-03-12 20:30:43 -0300

    Classes e Objetos

---
+++ 6 - Estrutura de dados e Objetos/classes_objetos.js
@@ -0,0 +1,9 @@
+class formaDeBolo{
+    constructor(saborDaMassa, saborRecheio){
+        this.saborDaMassa = saborDaMassa
+        this.saborRecheio = saborRecheio
+    }
+}
+
+let boloFesta = new formaDeBolo("Massa de chocolate", "Recheiro de Nutela")
+console.log(boloFesta)
